70154efab73e37b6d4abec5e868a584e24085380
张亚飞 <672233580@qq.com>
2021-09-03 13:36:54 +0800

    Initial Commit

---
+++ ShoeUI0903/ShoeUI0903App.swift
@@ -0,0 +1,17 @@
+//
+//  ShoeUI0903App.swift
+//  ShoeUI0903
+//
+//  Created by 张亚飞 on 2021/9/3.
+//
+
+import SwiftUI
+
+@main
+struct ShoeUI0903App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
